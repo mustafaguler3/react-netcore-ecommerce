c058c0cb798ef1a0d8f16d600bede7dc5ab5a89a
mustafaguler3 <mustafa.glr@hotmail.com>
2023-04-17 12:15:48 +0300

    added used async methods in products controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,32 @@
-﻿namespace API.Controllers
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
 {
-    public class ProductsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
     }
 }
