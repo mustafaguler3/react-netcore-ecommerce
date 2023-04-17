cfdc797e7ad1f9f163d039dbc1c34ba2ab7bc85d
mustafaguler3 <mustafa.glr@hotmail.com>
2023-04-17 12:42:32 +0300

    added front-end section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [products,setProducts] = useState([
+    {name:"product1",price:200},
+    {name:"product2",price:300}
+  ])
+
+  function addProduct() 
+  {  
+    setProducts(prevState => [...prevState,{name:"ms"+prevState.length + 1,price:(prevState.length*100)+100}])
+  }
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/products")
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:"green"}}>mustafa</h1>
+      <ul>
+        {products.map((item,index) => {
+          return <li key={index}>{item.name} - {item.price}</li>
+        })}
+      </ul>
+      <button style={{color:"red"}} onClick={()=>addProduct()}>Add Product</button>
     </div>
   );
 }
