11784f7fc289baa14c54fd95db94b87a90153a4a
mustafaguler3 <mustafa.glr@hotmail.com>
2023-04-17 11:22:09 +0300

    added product class and efcore

---
+++ Entities/Product.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
