2e3d85e27595f04cdca396b637679f7be07797fe
mustafaguler3 <mustafa.glr@hotmail.com>
2023-04-28 11:49:09 +0300

    refactoring catalog page design

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,33 +1,115 @@
-import { Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Grid,
+  Pagination,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
+import {
+  fetchFilters,
+  fetchProductsAsync,
+  productSelectors,
+} from "./catalogSlice";
 
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price - High to low" },
+  { value: "price", label: "Price - Low to high" },
+];
 
 export default function Catalog() {
   //const [products, setProducts] = useState<Product[]>([]);
   const products = useAppSelector(productSelectors.selectAll);
-  const {productsLoaded,status,filtersLoaded} = useAppSelector(state => state.catalog)
-  const dispatch = useAppDispatch()
-  
+  const { productsLoaded, status, filtersLoaded ,brands,types} = useAppSelector(
+    (state) => state.catalog
+  );
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if(!productsLoaded) dispatch(fetchProductsAsync());
-  }, [productsLoaded,dispatch]);
+    if (!productsLoaded) dispatch(fetchProductsAsync());
+  }, [productsLoaded, dispatch]);
 
   useEffect(() => {
-    if(!filtersLoaded) dispatch(fetchFilters())
-  }, [dispatch,filtersLoaded]);
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded]);
 
-  if(status.includes("pending")) return <LoadingComponent message="Loading products"/>
+  if (status.includes("pending"))
+    return <LoadingComponent message="Loading products" />;
 
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container spacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <TextField label="Search products" variant="outlined" fullWidth />
+        </Paper>
+
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl>
+            <RadioGroup>
+              {sortOptions.map(({ value, label }) => (
+                <FormControlLabel
+                  value={value}
+                  control={<Radio />}
+                  label={label}
+                  key={value}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </Paper>
+
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {brands.map(brand =>(
+              <FormControlLabel
+              control={<Checkbox />}
+              label={brand} key={brand}
+            />  
+            ))}
+            
+          </FormGroup>
+        </Paper>
+
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {types.map(type =>(
+              <FormControlLabel
+              control={<Checkbox />}
+              label={type} key={type}
+            />  
+            ))}
+            
+          </FormGroup>
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+
+      <Grid item xs={3} />
+      <Grid item xs={9}>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Typography>
+              Displaying 1-6 of 20 items
+            </Typography>
+            <Pagination color="secondary" size="large" count={10} page={2}/>
+          </Box>
+      </Grid>
+    </Grid>
   );
 }
