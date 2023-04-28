0874f55bfdbc86513349344cad1986ab3df27d38
mustafaguler3 <mustafa.glr@hotmail.com>
2023-04-28 10:57:43 +0300

    add filter endpoint on productscontroller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -18,7 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> GetProducts(ProductParams productParams)
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
             var query = _context.Products
                 .Sort(productParams.OrderBy)
@@ -28,7 +28,7 @@ namespace API.Controllers
 
             var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
 
-            Response.Headers.Add("pagination", JsonSerializer.Serialize(products.MetaData));
+            Response.AddPaginationHeader(products.MetaData);
 
             return products;
         }
---
+++ Extensions/HttpExtentions.cs
@@ -0,0 +1,18 @@
+﻿using API.RequestHelpers;
+using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtentions
+    {
+        public static void AddPaginationHeader(this HttpResponse response,MetaData metaData)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy= JsonNamingPolicy.CamelCase,
+            };
+            response.Headers.Add("pagination", JsonSerializer.Serialize(metaData,options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
